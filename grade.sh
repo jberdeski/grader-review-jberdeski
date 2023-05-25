1fcd9b8fcc8faab124a49426d1cac4e9cf01888b
jberdeski <jberdeski@ucsd.edu>
2023-05-25 13:16:17 -0700

    checked methods.. compile(?)

--- grade.sh
+++ grade.sh
@@ -11,8 +11,23 @@ echo 'Finished cloning'
 if [[ -f student-submission/ListExamples.java ]]
 then 
     cp student-submission/ListExamples.java ~/OneDrive/Documents/GitHub/list-examples-grader/grading-area
+    cp TestListExamples.java ~/OneDrive/Documents/GitHub/list-examples-grader/grading-area
 fi 
 
+gradingArea='grading-area/ListExamples.java'
+
+filterMethod='grep -c static List<String> filter(List<String> s, StringChecker sc) $gradingArea'
+mergeMethod='grep -c static List<String> merge(List<String> list1, List<String> list2) $gradingArea'
+
+if [[ $filterMethod == 0 ]] || [[ $mergeMethod == 0 ]]
+then
+    echo 'Missing method(s)'
+else 
+    echo 'Methods present'
+fi
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" grading-area/ListExamples.java grading-area/TestListExamples.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore ListExamples TestListExamples
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
